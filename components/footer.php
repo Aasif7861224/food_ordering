a7d4cd3fb3aca9cf64aa6658e4543b0c6a10ced8
Aasif7861224 <aasifpirjade0@gmail.com>
2025-03-13 11:43:34 +0530

    final commit with no errors

--- components/footer.php
+++ components/footer.php
@@ -11,21 +11,21 @@
       <div class="box">
          <img src="images/clock-icon.png" alt="">
          <h3>opening hours</h3>
-         <p>00:07am to 00:10pm</p>
+         <p>07:00am to 10:00pm</p>
       </div>
 
       <div class="box">
          <img src="images/map-icon.png" alt="">
          <h3>our address</h3>
-         <a href="#">Kurunegala, Sri Lanka - Bauddhaloka Road</a>
-         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3955.8479582324317!2d80.3576487!3d7.482035799999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae33a227cc88679%3A0x9b315d38cde4b45e!2sBauddhaloka%20Rd%2C%20Kurunegala!5e0!3m2!1sen!2slk!4v1704283880683!5m2!1sen!2slk" width="250" height="250" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+         <a href="#">Khanapur,Maharashtra, India </a>
+         <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d192.59359454876318!2d74.71186875095027!3d17.263922896152764!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1sen!2sin!4v1741842215508!5m2!1sen!2sin" width="250" height="250" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
       </div>
 
       <div class="box">
          <img src="images/phone-icon.png" alt="">
          <h3>our number</h3>
-         <a href="tel:1234567890">123-456-7890</a>
-         <a href="tel:1112223333">111-222-3333</a>
+         <a href="tel:9226890954">922-689-0954</a>
+         <a href="tel:9921142077">992-114-2077</a>
       </div>
 
       
